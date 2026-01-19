2993f2278ae37a3ff1008880a2191e1bffd64ddd
hanmin-oh <hanmin.paul.oh@gmail.com>
2026-01-19 20:20:10 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import PageMain from './PageMain'
+import './App.css'
+
+function App() {
+    return (
+        <div className="layout">
+            <header className="menu">MENU</header>
+
+            <PageMain />
+
+            <footer className="footer">FOOTER</footer>
+        </div>
+    )
+}
+
+export default App
---
+++ src/PageMain.jsx
@@ -0,0 +1,14 @@
+function PageMain() {
+    return (
+        <div className="content">
+            <section className="top">TOP</section>
+
+            <div className="middle">
+                <aside className="left">LEFT</aside>
+                <main className="right">RIGHT</main>
+            </div>
+        </div>
+    )
+}
+
+export default PageMain
